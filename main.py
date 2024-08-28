c548df96042f09e221010cbe73b200819c07cbe2
AlsammanAlsamman <smahmoud@ageri.sci.eg>
2024-08-28 15:51:05 +0300

    new

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
+from seqmod.seqIO import read_fasta
 
-
-
-
-
-
-
-
-
-
-
+fastaFile="exampledata/seqmultiline.fasta"
+myseqs = read_fasta(fastaFile)
+print(myseqs)
 # if __name__ == '__main__':
 #     pass
---
+++ seqmod/seqIO.py
@@ -0,0 +1,19 @@
+def read_fasta(fastaFile):
+    seqs = {} # key: header, value: sequence
+    with open(fastaFile) as f:
+        header=None
+        for line in f:
+            # remove whitespace
+            line=line.strip()
+            # skip empty lines
+            if len(line)==0:
+                continue
+            # check for header
+            if ">"  == line[0]:
+                header=line
+                header=header[1:]
+                header=header.split()[0]
+                seqs[header] = ""
+            else:
+                seqs[header] += line
+    return seqs
